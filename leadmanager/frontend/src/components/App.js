17fb7fe404b584c46794518ee7a1f818d734a08e
Sanel Zulic <szulic@capeannenterprises.com>
2020-02-05 13:31:11 +0100

    Added alerts with messages for errors, add and delete

--- leadmanager/frontend/src/components/App.js
+++ leadmanager/frontend/src/components/App.js
@@ -3,19 +3,30 @@ import ReactDOM from "react-dom";
 
 import Header from "./layout/Header";
 import Dashboard from "./leads/Dashboard";
+import Alerts from "./layout/Alerts";
 import { Provider } from "react-redux";
+import { Provider as AlertProvider } from "react-alert";
+import AlertTemplate from "react-alert-template-basic";
 import store from "../store";
+
+const alertOptions = {
+    timeout: 3000,
+    position: "top center"
+};
 
 class App extends Component {
     render() {
         return (
             <Provider store={store}>
-                <Fragment>
-                    <Header />
-                    <div className="container">
-                        <Dashboard />
-                    </div>
-                </Fragment>
+                <AlertProvider template={AlertTemplate} {...alertOptions}>
+                    <Fragment>
+                        <Header />
+                        <Alerts />
+                        <div className="container">
+                            <Dashboard />
+                        </div>
+                    </Fragment>
+                </AlertProvider>
             </Provider>
         );
     }

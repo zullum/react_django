16fc51537c20090acb5e6bf4faec2f089c93abff
Sanel Zulic <szulic@capeannenterprises.com>
2020-02-05 09:37:52 +0100

    React Redux Django CRUD example

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node-modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            }
+        ]
+    }
+};

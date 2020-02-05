7d12c9c5fc6478deb4af2d4666360cd3c84cc3ac
Sanel Zulic <szulic@capeannenterprises.com>
2020-02-05 17:19:44 +0100

    Added backend authorization in django

--- leadmanager/leads/api.py
+++ leadmanager/leads/api.py
@@ -4,9 +4,18 @@ from .serializers import LeadSerializer
 
 # Lead ViewSet
 
+
 class LeadViewSet(viewsets.ModelViewSet):
-    queryset = Lead.objects.all()
+    # queryset = Lead.objects.all()
     permission_classes = [
-        permissions.AllowAny
+        # permissions.AllowAny
+        permissions.IsAuthenticated
     ]
+
     serializer_class = LeadSerializer
+
+    def get_queryset(self):
+        return self.request.user.leads.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
